ad09c104c2200fb6efbd3645fa30ec9df82fb8b1
bittencourtjulival <julival.bitencourt@gmail.com>
2025-03-16 21:47:22 -0300

    stone: bt: fixed pairing issues on media devices

--- device.mk
+++ device.mk
@@ -78,8 +78,10 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    audio.bluetooth.default \
     android.hardware.bluetooth@1.1.vendor \
     com.dsi.ant@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
