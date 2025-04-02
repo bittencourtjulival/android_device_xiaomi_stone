a4bb22de1dcee2adde6f6e6ca474a2bc1831c21f
bittencourtjulival <julival.bitencourt@gmail.com>
2025-04-01 23:03:33 -0300

    stone: media: build opitionally Dolby

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Vendor Dolby
+$(call inherit-product-if-exists, vendor/dolby/dolby.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
 
