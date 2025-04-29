93064c3e095653261d2aef6940ca39b0efe9a98a
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:24:23 +0100

    stone: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- device.mk
+++ device.mk
@@ -202,6 +202,9 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_stone)
+
 # Input
 PRODUCT_PACKAGES += \
     gpio-keys.kl \
