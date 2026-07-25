61e260615b7d04c5feac6cf905f84f0ffdd99362
bittencourtjulival <julival.bitencourt@gmail.com>
2026-07-25 16:34:22 -0300

    stone: parts: Migrate DozeSettingsFragment to AndroidX APIs
    Update DozeSettingsFragment to use the modern AndroidX
    preference and fragment APIs by extending
    SettingsBasePreferenceFragment, replacing deprecated
    PreferenceFragment/DialogFragment classes, and updating
    preference initialization and dialog handling.
    
    Signed-off-by: Julival Bittencourt <julival.bitencourt@gmail.com>

--- parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -20,28 +20,27 @@ package org.lineageos.settings.doze;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import androidx.fragment.app.DialogFragment;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
-import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 
 import com.android.settingslib.widget.MainSwitchPreference;
+import com.android.settingslib.widget.SettingsBasePreferenceFragment;
 
 import org.lineageos.settings.R;
 import org.lineageos.settings.utils.FileUtils;
 
-public class DozeSettingsFragment extends PreferenceFragment
-        implements OnPreferenceChangeListener, OnCheckedChangeListener {
+public class DozeSettingsFragment extends SettingsBasePreferenceFragment
+        implements Preference.OnPreferenceChangeListener,
+                   CompoundButton.OnCheckedChangeListener {
     private MainSwitchPreference mSwitchBar;
 
     private SwitchPreference mAlwaysOnDisplayPreference;
@@ -51,7 +50,7 @@ public class DozeSettingsFragment extends PreferenceFragment
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        addPreferencesFromResource(R.xml.doze_settings);
+        setPreferencesFromResource(R.xml.doze_settings, rootKey);
 
         SharedPreferences prefs =
                 getActivity().getSharedPreferences("doze_settings", Activity.MODE_PRIVATE);
@@ -151,6 +150,6 @@ public class DozeSettingsFragment extends PreferenceFragment
 
     private void showHelp() {
         HelpDialogFragment fragment = new HelpDialogFragment();
-        fragment.show(getFragmentManager(), "help_dialog");
+        fragment.show(getParentFragmentManager(), "help_dialog");
     }
 }
