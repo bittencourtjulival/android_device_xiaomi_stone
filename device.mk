b9e8c90a5eea17a5682995c7bbbd6c8f92a6097d
Tashar02 <tashfinshakeerrhythm@gmail.com>
2026-08-04 13:59:23 -0300

    stone: Use new location of dex2oat boot image profile config/boot-image-profile.txt has been dropped in favor of boot/boot-image-profile.txt for all since Evolution-X/frameworks_base@6431bd0
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- device.mk
+++ device.mk
@@ -296,7 +296,7 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 # Use a profile based boot image for this device. Note that this is currently a generic
 # profile and not Android Go optimized.
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Partitions
 PRODUCT_PACKAGES += \
