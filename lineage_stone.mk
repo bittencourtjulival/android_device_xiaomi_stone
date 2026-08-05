cf68df9ae7a232200e15f3cac1366337f645b05a
bittencourtjulival <julival.bitencourt@gmail.com>
2026-08-05 10:04:29 -0300

    stone: CleanUp Rising stuffs
    Signed-off-by: Julival Bittencourt <julival.bitencourt@gmail.com>

--- lineage_stone.mk
+++ lineage_stone.mk
@@ -15,9 +15,6 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# rising Maintainer
-RISING_MAINTAINER := Julival Bittencourt
-
 # Blur
 TARGET_ENABLE_BLUR := true
 
@@ -34,20 +31,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 12 5G
 PRODUCT_NAME := lineage_stone
 
-# Lawnchair
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
-
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-TARGET_DEFAULT_PIXEL_LAUNCHER = false
-
-PRODUCT_SYSTEM_PROPERTIES += \
-    persist.sys.default_launcher=0 \
-    persist.sys.quickswitch_pixel_shipped=1
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sunstone_eea-user 14 UKQ1.240624.001 OS2.0.9.0.UMQEUXM release-keys" \
     BuildFingerprint=Redmi/sunstone_eea/sunstone:14/UKQ1.240624.001/OS2.0.9.0.UMQEUXM:user/release-keys \
-    DeviceProduct=sunstone \
-    RisingChipset="Snapdragon 695/4 Gen1" \
-    RisingMaintainer="Julival Bittencourt"
+    DeviceProduct=sunstone
