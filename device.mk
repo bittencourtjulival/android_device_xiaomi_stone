2d7141e4ff18be49bcf4ca8c8503ef04ccea7344
bittencourtjulival <julival.bitencourt@gmail.com>
2026-08-05 18:18:11 -0300

    stone: Add missing libion legacy_impl Soong config
    Enable libion legacy implementation through Soong configuration to
    match the required build setup and fix missing legacy ION support.
    
    Signed-off-by: Julival Bittencourt <julival.bitencourt@gmail.com>

--- device.mk
+++ device.mk
@@ -207,6 +207,9 @@ PRODUCT_PACKAGES += \
 # Init
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_stone)
 
+# ION
+$(call soong_config_set_bool,libion,legacy_impl,true)
+
 # Input
 PRODUCT_PACKAGES += \
     gpio-keys.kl \
